94c27315169bffb92dc5528b2e5f01b76a3ccbd5
Andres Castro <andrescastro2011@hotmail.com>
2022-09-24 19:18:09 -0300

    page acceso publico

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import StudyApp from "./StudyApp";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import AccesoPublico from "./onBoarding/pages/AccesoPublico";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <React.StrictMode>
-      <StudyApp />
+      <AccesoPublico />
     </React.StrictMode>
   </BrowserRouter>
 );
---
+++ src/onBoarding/pages/AccesoPublico.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AccesoPublico = () => {
+  return (
+    <div>AccesoPublico</div>
+  )
+}
+
+export default AccesoPublico
