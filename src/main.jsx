b06f29a3d85d98e5e5a2b435b8b18ef60259bcf6
Andres Castro <andrescastro2011@hotmail.com>
2022-09-26 00:28:10 -0300

    navbar

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,12 @@ import ReactDOM from "react-dom/client";
 import StudyApp from "./StudyApp";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import RutasUniversidades from "./onBoarding/components/NavBar/RutasUniversidades";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <React.StrictMode>
-      <RutasUniversidades />
+      <StudyApp/>
     </React.StrictMode>
   </BrowserRouter>
 );
