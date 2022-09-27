94e3831049b949e2c43bc218b23ac61e969d9ec5
Andres Castro <andrescastro2011@hotmail.com>
2022-09-26 22:58:45 -0300

    se agrego el componente de card

--- src/onBoarding/components/NavBar/NivelAcademico.jsx
+++ src/onBoarding/components/NavBar/NivelAcademico.jsx
@@ -6,125 +6,124 @@ import CargaLlena from './Icons/CargaLlena'
 import Carga1Barra from './Icons/Carga1Barra'
 import Carga3Barras from './Icons/Carga3Barras'
 import Carga4Barras from './Icons/Carga4Barras'
+import Card from '../Card'
 
 const NivelAcademico = () => {
     return (
         <>
             <h1 className='titulo'>Las universidades de tu futura carrera según <br /> <span className='bold'>el nivel academico</span> </h1>
 
-        <div className='grid'> 
+            <div className='grid'> 
 
-            <div className='card'>
-                <div className='cardContenido'>
-                    <h2 className='cardNumero'>1</h2>
-                    <h3>Ingenieria en Informatica</h3>
+                <div className='card'>
+                    <Card 
+                        indice = '1' 
+                        titulo = 'Ingenieria en informatica' 
+                        url = 'https://i.postimg.cc/pdsZVkf1/image-30.png'
+                        carga = 'CargaLlena'
+                    />
+
+                    <div className='cargaContainer'>
+                        <CargaLlena  texto= 'Nivel Academico' />
+                        <Carga1Barra texto= 'Acceso transporte publico' />
+                        <Carga4Barras texto="Nivel profesor" />
+                        <Carga3Barras texto= 'Ambiente'/>
+                        <Carga3Barras texto='Demanda Horaria'/>
+                    </div> 
                 </div>
-                    <button className='verUniversidad'>Ver Universidad</button>
-                <img className='img' src='https://i.postimg.cc/pdsZVkf1/image-30.png' alt="imagen" />
-                
-                <div className='cargaContainer'>
-                    <CargaLlena  texto= 'Nivel Academico' />
-                    <Carga1Barra texto= 'Acceso transporte publico' />
-                    <Carga4Barras texto="Nivel profesor" />
-                    <Carga3Barras texto= 'Ambiente'/>
-                    <Carga3Barras texto='Demanda Horaria'/>
+
+                <div className='card'>
+                    <Card 
+                        indice = '1' 
+                        titulo = 'Ingenieria en informatica' 
+                        url = 'https://i.postimg.cc/pdsZVkf1/image-30.png'
+                        carga = 'CargaLlena'
+                    />
+
+                    <div className='cargaContainer'>
+                        <CargaLlena  texto= 'Nivel Academico' />
+                        <Carga1Barra texto= 'Acceso transporte publico' />
+                        <Carga4Barras texto="Nivel profesor" />
+                        <Carga3Barras texto= 'Ambiente'/>
+                        <Carga3Barras texto='Demanda Horaria'/>
+                    </div> 
+                </div>
+
+                <div className='card'>
+                    <Card 
+                        indice = '1' 
+                        titulo = 'Ingenieria en informatica' 
+                        url = 'https://i.postimg.cc/pdsZVkf1/image-30.png'
+                        carga = 'CargaLlena'
+                    />
+
+                    <div className='cargaContainer'>
+                        <CargaLlena  texto= 'Nivel Academico' />
+                        <Carga1Barra texto= 'Acceso transporte publico' />
+                        <Carga4Barras texto="Nivel profesor" />
+                        <Carga3Barras texto= 'Ambiente'/>
+                        <Carga3Barras texto='Demanda Horaria'/>
+                    </div> 
+                </div>
+
+                <div className='card'>
+                    <Card 
+                        indice = '1' 
+                        titulo = 'Ingenieria en informatica' 
+                        url = 'https://i.postimg.cc/pdsZVkf1/image-30.png'
+                        carga = 'CargaLlena'
+                    />
+
+                    <div className='cargaContainer'>
+                        <CargaLlena  texto= 'Nivel Academico' />
+                        <Carga1Barra texto= 'Acceso transporte publico' />
+                        <Carga4Barras texto="Nivel profesor" />
+                        <Carga3Barras texto= 'Ambiente'/>
+                        <Carga3Barras texto='Demanda Horaria'/>
+                    </div> 
+                </div>
+
+                <div className='card'>
+                    <Card 
+                        indice = '1' 
+                        titulo = 'Ingenieria en informatica' 
+                        url = 'https://i.postimg.cc/pdsZVkf1/image-30.png'
+                        carga = 'CargaLlena'
+                    />
+
+                    <div className='cargaContainer'>
+                        <CargaLlena  texto= 'Nivel Academico' />
+                        <Carga1Barra texto= 'Acceso transporte publico' />
+                        <Carga4Barras texto="Nivel profesor" />
+                        <Carga3Barras texto= 'Ambiente'/>
+                        <Carga3Barras texto='Demanda Horaria'/>
+                    </div> 
+                </div>
+
+                <div className='card'>
+                    <Card 
+                        indice = '1' 
+                        titulo = 'Ingenieria en informatica' 
+                        url = 'https://i.postimg.cc/pdsZVkf1/image-30.png'
+                        carga = 'CargaLlena'
+                    />
+
+                    <div className='cargaContainer'>
+                        <CargaLlena  texto= 'Nivel Academico' />
+                        <Carga1Barra texto= 'Acceso transporte publico' />
+                        <Carga4Barras texto="Nivel profesor" />
+                        <Carga3Barras texto= 'Ambiente'/>
+                        <Carga3Barras texto='Demanda Horaria'/>
+                    </div> 
                 </div>
             </div>
 
-            <div className='card'>
-                <div className='cardContenido'>
-                    <h2 className='cardNumero'>1</h2>
-                    <h3>Ingenieria en Informatica</h3>
-                </div>
-                    <button className='verUniversidad'>Ver Universidad</button>
-                <img className='img' src='https://i.postimg.cc/pdsZVkf1/image-30.png' alt="imagen" />
-                
-                <div className='cargaContainer'>
-                    <CargaLlena  texto= 'Nivel Academico' />
-                    <Carga1Barra texto= 'Acceso transporte publico' />
-                    <Carga4Barras texto="Nivel profesor" />
-                    <Carga3Barras texto= 'Ambiente'/>
-                    <Carga3Barras texto='Demanda Horaria'/>
-                </div>
+            <div className='buttonVerMas'>
+                <Link className='verMas' to="NivelAcademico">Ver Mas</Link>
             </div>
 
-            <div className='card'>
-                <div className='cardContenido'>
-                    <h2 className='cardNumero'>1</h2>
-                    <h3>Ingenieria en Informatica</h3>
-                </div>
-                    <button className='verUniversidad'>Ver Universidad</button>
-                <img className='img' src='https://i.postimg.cc/pdsZVkf1/image-30.png' alt="imagen" />
-                
-                <div className='cargaContainer'>
-                    <CargaLlena  texto= 'Nivel Academico' />
-                    <Carga1Barra texto= 'Acceso transporte publico' />
-                    <Carga4Barras texto="Nivel profesor" />
-                    <Carga3Barras texto= 'Ambiente'/>
-                    <Carga3Barras texto='Demanda Horaria'/>
-                </div>
-            </div>
-
-            <div className='card'>
-                <div className='cardContenido'>
-                    <h2 className='cardNumero'>1</h2>
-                    <h3>Ingenieria en Informatica</h3>
-                </div>
-                    <button className='verUniversidad'>Ver Universidad</button>
-                <img className='img' src='https://i.postimg.cc/pdsZVkf1/image-30.png' alt="imagen" />
-                
-                <div className='cargaContainer'>
-                    <CargaLlena  texto= 'Nivel Academico' />
-                    <Carga1Barra texto= 'Acceso transporte publico' />
-                    <Carga4Barras texto="Nivel profesor" />
-                    <Carga3Barras texto= 'Ambiente'/>
-                    <Carga3Barras texto='Demanda Horaria'/>
-                </div>
-            </div>
-
-            <div className='card'>
-                <div className='cardContenido'>
-                    <h2 className='cardNumero'>1</h2>
-                    <h3>Ingenieria en Informatica</h3>
-                </div>
-                    <button className='verUniversidad'>Ver Universidad</button>
-                <img className='img' src='https://i.postimg.cc/pdsZVkf1/image-30.png' alt="imagen" />
-                
-                <div className='cargaContainer'>
-                    <CargaLlena  texto= 'Nivel Academico' />
-                    <Carga1Barra texto= 'Acceso transporte publico' />
-                    <Carga4Barras texto="Nivel profesor" />
-                    <Carga3Barras texto= 'Ambiente'/>
-                    <Carga3Barras texto='Demanda Horaria'/>
-                </div>
-            </div>
-
-            <div className='card'>
-                <div className='cardContenido'>
-                    <h2 className='cardNumero'>1</h2>
-                    <h3>Ingenieria en Informatica</h3>
-                </div>
-                    <button className='verUniversidad'>Ver Universidad</button>
-                <img className='img' src='https://i.postimg.cc/pdsZVkf1/image-30.png' alt="imagen" />
-                
-                <div className='cargaContainer'>
-                    <CargaLlena  texto= 'Nivel Academico' />
-                    <Carga1Barra texto= 'Acceso transporte publico' />
-                    <Carga4Barras texto="Nivel profesor" />
-                    <Carga3Barras texto= 'Ambiente'/>
-                    <Carga3Barras texto='Demanda Horaria'/>
-                </div>
-            </div>
-
-        </div>
-
-        <div className='buttonVerMas'>
-            <Link className='verMas' to="NivelAcademico">Ver Mas</Link>
-        </div>
-
-        <p>ACA VA EL FOOTER!!</p>
-        
-    </>
+            <p>ACA VA EL FOOTER!!</p>
+        </>
     )
 }
 
