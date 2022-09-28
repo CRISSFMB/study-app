6c65195cf522f98fd03ee55cf07117fa98b8e79c
luisnava6667 <luisnavarro19890603@gmail.com>
2022-09-28 09:10:18 -0300

    Footer

--- src/home/pages/HomeScreen.jsx
+++ src/home/pages/HomeScreen.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Footer from "../../Footer/Footer";
 
 export const HomeScreen = () => {
   return (
     <div>
       <h1>HomeScreen</h1>
+      <Footer/>
     </div>
   );
 };
