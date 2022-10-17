d58e94e59533d145fb61b7416d241e2cf22af036
Andres Castro <andrescastro2011@hotmail.com>
2022-10-17 20:16:18 -0300

    vista previa

--- src/onBoarding/components/NavBar/NivelAcademico.jsx
+++ src/onBoarding/components/NavBar/NivelAcademico.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './NivelAcademico.css'
 import {Link} from 'react-router-dom'
-import Icon from './Icons/CargaLlena'
+
 import CargaLlena from './Icons/CargaLlena'
 import Carga1Barra from './Icons/Carga1Barra'
 import Carga3Barras from './Icons/Carga3Barras'
