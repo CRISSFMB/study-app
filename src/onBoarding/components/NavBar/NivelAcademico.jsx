097f49328e85cea683d77c3de765d8b6b1a4f4d2
Andres Castro <andrescastro2011@hotmail.com>
2022-10-18 19:31:08 -0300

    se obtiene la data atravez de un json

--- src/onBoarding/components/NavBar/NivelAcademico.jsx
+++ src/onBoarding/components/NavBar/NivelAcademico.jsx
@@ -7,9 +7,11 @@ import Carga1Barra from './Icons/Carga1Barra'
 import Carga3Barras from './Icons/Carga3Barras'
 import Carga4Barras from './Icons/Carga4Barras'
 import Card from '../Card'
-import NavBar from './NavBar'
+
+import api from '../../../api.json'
 
 const NivelAcademico = () => {
+
     return (
         <>
             <h1 className='titulo'>Las universidades de tu futura carrera según <br /> <span className='bold'>el nivel academico</span> </h1>
@@ -17,104 +19,72 @@ const NivelAcademico = () => {
             <div className='grid'> 
 
                 <div className='card'>
-                    <Card 
-                        indice = '1' 
-                        titulo = 'Ingenieria en informatica' 
-                        url = 'https://i.postimg.cc/pdsZVkf1/image-30.png'
-                        carga = 'CargaLlena'
-                    />
-
+                    <Card indice = {api[0].indice} titulo = {api[0].titulo} url = {api[0].url}/>
                     <div className='cargaContainer'>
-                        <CargaLlena  texto= 'Nivel Academico' />
-                        <Carga1Barra texto= 'Acceso transporte publico' />
-                        <Carga4Barras texto="Nivel profesor" />
-                        <Carga3Barras texto= 'Ambiente'/>
-                        <Carga3Barras texto='Demanda Horaria'/>
+                        <CargaLlena  texto= {api[0].carga1} />
+                        <Carga1Barra texto= {api[0].carga2}  />
+                        <Carga4Barras texto={api[0].carga3}   />
+                        <Carga3Barras texto= {api[0].carga4}  />
+                        <Carga3Barras texto= {api[0].carga5}  />
                     </div> 
                 </div>
 
                 <div className='card'>
-                    <Card 
-                        indice = '1' 
-                        titulo = 'Ingenieria en informatica' 
-                        url = 'https://i.postimg.cc/pdsZVkf1/image-30.png'
-                        carga = 'CargaLlena'
-                    />
+                    <Card indice = {api[1].indice} titulo = {api[1].titulo} url = {api[1].url} />
+                    <div className='cargaContainer'>
+                        <CargaLlena  texto= {api[0].carga1} />
+                        <Carga3Barras texto= {api[0].carga2}  />
+                        <Carga3Barras texto={api[0].carga3}   />
+                        <Carga4Barras texto= {api[0].carga4}  />
+                        <Carga4Barras texto= {api[0].carga5}  />
+                    </div>
+                </div>
+
+                <div className='card'>
+                    <Card indice = {api[2].indice} titulo = {api[2].titulo} url = {api[2].url} />
 
                     <div className='cargaContainer'>
-                        <CargaLlena  texto= 'Nivel Academico' />
-                        <Carga1Barra texto= 'Acceso transporte publico' />
-                        <Carga4Barras texto="Nivel profesor" />
-                        <Carga3Barras texto= 'Ambiente'/>
-                        <Carga3Barras texto='Demanda Horaria'/>
+                        <CargaLlena  texto= {api[0].carga1} />
+                        <Carga4Barras texto= {api[0].carga2}  />
+                        <Carga4Barras texto={api[0].carga3}   />
+                        <Carga3Barras texto= {api[0].carga4}  />
+                        <Carga3Barras texto= {api[0].carga5}  />
                     </div> 
                 </div>
 
                 <div className='card'>
-                    <Card 
-                        indice = '1' 
-                        titulo = 'Ingenieria en informatica' 
-                        url = 'https://i.postimg.cc/pdsZVkf1/image-30.png'
-                        carga = 'CargaLlena'
-                    />
+                    <Card indice = {api[3].indice} titulo = {api[3].titulo} url = {api[3].url} />
 
                     <div className='cargaContainer'>
-                        <CargaLlena  texto= 'Nivel Academico' />
-                        <Carga1Barra texto= 'Acceso transporte publico' />
-                        <Carga4Barras texto="Nivel profesor" />
-                        <Carga3Barras texto= 'Ambiente'/>
-                        <Carga3Barras texto='Demanda Horaria'/>
+                        <CargaLlena  texto= {api[0].carga1} />
+                        <Carga3Barras texto= {api[0].carga2}  />
+                        <Carga3Barras texto={api[0].carga3}   />
+                        <Carga4Barras texto= {api[0].carga4}  />
+                        <Carga1Barra texto= {api[0].carga5}  />
                     </div> 
                 </div>
 
                 <div className='card'>
-                    <Card 
-                        indice = '1' 
-                        titulo = 'Ingenieria en informatica' 
-                        url = 'https://i.postimg.cc/pdsZVkf1/image-30.png'
-                        carga = 'CargaLlena'
-                    />
+                    <Card indice = {api[4].indice} titulo = {api[4].titulo} url = {api[4].url} />
 
                     <div className='cargaContainer'>
-                        <CargaLlena  texto= 'Nivel Academico' />
-                        <Carga1Barra texto= 'Acceso transporte publico' />
-                        <Carga4Barras texto="Nivel profesor" />
-                        <Carga3Barras texto= 'Ambiente'/>
-                        <Carga3Barras texto='Demanda Horaria'/>
+                        <CargaLlena  texto= {api[0].carga1} />
+                        <Carga4Barras texto= {api[0].carga2}  />
+                        <Carga4Barras texto={api[0].carga3}   />
+                        <Carga3Barras texto= {api[0].carga4}  />
+                        <Carga3Barras texto= {api[0].carga5}  />
                     </div> 
                 </div>
 
                 <div className='card'>
-                    <Card 
-                        indice = '1' 
-                        titulo = 'Ingenieria en informatica' 
-                        url = 'https://i.postimg.cc/pdsZVkf1/image-30.png'
-                        carga = 'CargaLlena'
-                    />
+                    <Card indice = {api[5].indice} titulo = {api[5].titulo} url = {api[5].url} />
 
                     <div className='cargaContainer'>
-                        <CargaLlena  texto= 'Nivel Academico' />
-                        <Carga1Barra texto= 'Acceso transporte publico' />
-                        <Carga4Barras texto="Nivel profesor" />
-                        <Carga3Barras texto= 'Ambiente'/>
-                        <Carga3Barras texto='Demanda Horaria'/>
-                    </div> 
-                </div>
-
-                <div className='card'>
-                    <Card 
-                        indice = '1' 
-                        titulo = 'Ingenieria en informatica' 
-                        url = 'https://i.postimg.cc/pdsZVkf1/image-30.png'
-                        carga = 'CargaLlena'
-                    />
-
-                    <div className='cargaContainer'>
-                        <CargaLlena  texto= 'Nivel Academico' />
-                        <Carga1Barra texto= 'Acceso transporte publico' />
-                        <Carga4Barras texto="Nivel profesor" />
-                        <Carga3Barras texto= 'Ambiente'/>
-                        <Carga3Barras texto='Demanda Horaria'/>
+                        <CargaLlena  texto= {api[0].carga1} />
+                        <Carga1Barra texto= {api[0].carga2}  />
+                        <Carga1Barra texto={api[0].carga3}   />
+                        <Carga3Barras texto= {api[0].carga4}  />
+                        <Carga3Barras texto= {api[0].carga5}  />
                     </div> 
                 </div>
             </div>
