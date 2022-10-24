9994ee6ca9db07a1a68b90a59a7c9f18629efea8
Santi <santi.cocciardi@gmail.com>
2022-10-24 13:22:16 -0300

    Skeleton vista Universidades

--- src/home/components/Carousel/Carousel.jsx
+++ src/home/components/Carousel/Carousel.jsx
@@ -15,10 +15,56 @@ import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
 import CardSlider from "./CardSlider";
 import { getDataUniversity } from "../../../Firebase/firebase-utils";
 
+const styleContainer = {
+  padding: "10px",
+  width: "90%",
+  maxWidth: "446px",
+  border: "1px solid black",
+  marginTop: "2rem"
+}
 
+const styleHeader = {
+  display: "flex",
+  justifyContent: "space-around"
+}
+
+const Skeleton = () => {
+
+  return <>
+    <div style={styleContainer}>
+      <div style={styleHeader} >
+        <div className="skeleton" style={{ width: "10px", height: "10px" }}> </div>
+        <div className="skeleton" style={{ width: "300px", height: "15px" }}> </div>
+        <div className="skeleton" style={{ width: "50px", height: "10px" }}></div>
+      </div>
+      <img className="skeleton" style={{ width: "100%", height: "180px", marginTop: "1px" }}></img>
+
+      <div className="card__footer">
+        <div className="box">
+          <span className="skeleton" style={{ width: "15px", height: "15px" }}></span>
+          <span className="skeleton" style={{ width: "60px", height: "15px", marginTop: "8px" }}></span>
+        </div>
+        <div className="box">
+          <span className="skeleton" style={{ width: "15px", height: "15px" }}></span>
+          <span className="skeleton" style={{ width: "60px", height: "15px", marginTop: "8px" }}></span>
+
+        </div>
+        <div className="box">
+          <span className="skeleton" style={{ width: "30px", height: "15px"}}></span>
+          <span className="skeleton" style={{ width: "60px", height: "15px", marginTop: "8px" }}></span>
+        </div>
+        <div className="box">
+          <span className="skeleton" style={{ width: "30px", height: "15px"}}></span>
+          <span className="skeleton" style={{ width: "60px", height: "15px", marginTop: "8px" }}></span>
+        </div>
+      </div>
+    </div>
+
+  </>
+}
 
 const Carousel = () => {
-  
+
   const [data, setData] = useState()
 
   useEffect(() => {
@@ -50,7 +96,7 @@ const Carousel = () => {
             <CardSlider data={card} key={index} />
           </SwiperSlide>
         })
-          : <p className="carrosuel__loading">Cargando..</p>}
+          : <SwiperSlide><Skeleton /></SwiperSlide>}
     </Swiper>
   );
 };
